6ca96e2330a950e4ffb3d64424e67de868f00e25
Emre Şahiner <emre@workintech.com.tr>
2024-09-09 11:14:31 +0300

    add sample profile data

---
+++ src/data.js
@@ -0,0 +1,6 @@
+export const profiles = [
+  { avatar: "https://picsum.photos/200?0", name: "Emre" },
+  { avatar: "https://picsum.photos/200?1", name: "Duru" },
+  { avatar: "https://picsum.photos/200?2", name: "Gamze" },
+  { avatar: "https://picsum.photos/200?3", name: "Türkan" },
+];
