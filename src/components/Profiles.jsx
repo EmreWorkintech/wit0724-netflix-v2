6b5d3843f459a6184e358d06f7a13a1df470cbeb
Emre Şahiner <emre@workintech.com.tr>
2024-09-09 10:59:35 +0300

    add welcome page

---
+++ src/components/Profiles.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const ProfilesSection = styled.section`
+  height: 20vh;
+  border: 1px solid white;
+  width: 50%;
+`;
+
+function Profiles() {
+  return <ProfilesSection>Profiles</ProfilesSection>;
+}
+
+export default Profiles;
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,5 +1,37 @@
+import styled from "styled-components";
+import Profiles from "../components/Profiles";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
+const WelcomePage = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  font-weight: normal;
+  font-size: 2.5rem;
+`;
+
+const Button = styled(Link)`
+  border: 1px solid gray;
+  color: gray;
+  padding: 0.5rem 2rem;
+  text-decoration: none;
+  margin-top: 2rem;
+`;
+
 function Welcome() {
-  return <div>Welcome</div>;
+  return (
+    <WelcomePage>
+      <Title>Who's Watching</Title>
+      <Profiles />
+      <Button>Manage Profiles</Button>
+    </WelcomePage>
+  );
 }
 
 export default Welcome;
