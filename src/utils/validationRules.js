41c16076ce22b63eee46dad61223849321176d4e
Emre Şahiner <emre@workintech.com.tr>
2024-09-10 10:52:09 +0300

    add validation rules for elements

---
+++ src/utils/validationRules.js
@@ -0,0 +1,20 @@
+export const isValidEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+export const isValidPassword = (pass) => {
+  return (
+    pass.length >= 8 &&
+    pass.toLowerCase() !== pass &&
+    pass.toUpperCase() !== pass
+  );
+};
+
+export const isValidPasswordByRegex = (pass) => {
+  var re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+  return re.test(pass);
+};
