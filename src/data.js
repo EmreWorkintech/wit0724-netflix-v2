5b889f2b1c0179a6cfc02051524e7a93bdeb52e7
Emre Şahiner <emre@workintech.com.tr>
2024-09-10 10:51:59 +0300

    add genre list

--- src/data.js
+++ src/data.js
@@ -4,3 +4,21 @@ export const profiles = [
   { avatar: "https://picsum.photos/200?2", name: "Gamze" },
   { avatar: "https://picsum.photos/200?3", name: "Türkan" },
 ];
+
+export const genres = [
+  "action",
+  "adventure",
+  "animation",
+  "biography",
+  "crime",
+  "family",
+  "fantasy",
+  "history",
+  "horror",
+  "mystery",
+  "romance",
+  "scifi",
+  "sports",
+  "thriller",
+  "war",
+];
