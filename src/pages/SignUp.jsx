419d9f3ab0256efb897fd66bee01941e9f71c326
Emre Şahiner <emre@workintech.com.tr>
2024-09-10 10:52:40 +0300

    add form management and validation

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,26 @@
+import SignInForm from "../components/SignInForm";
+import Footer from "../layouts/Footer";
+import Header from "../layouts/Header";
+import styled from "styled-components";
+
+const PageBackground = styled.div`
+  background: white;
+  height: 100vh;
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
 function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <PageBackground>
+      <Header />
+      <SignInForm />
+      <Footer />
+    </PageBackground>
+  );
 }
 
 export default SignUp;
