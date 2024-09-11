b0ec2f78519f718de0d61f4128003b6f11c21b96
Emre Şahiner <emre@workintech.com.tr>
2024-09-11 10:50:34 +0300

    add new header UI for active Profile

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import UserPanel from "../components/UserPanel";
 import styled from "styled-components";
 
 const HeaderSection = styled.header`
@@ -6,9 +8,11 @@ const HeaderSection = styled.header`
   justify-content: space-between;
   align-items: center;
   padding: 1rem 2rem;
-  border-bottom: 1px solid gray;
+  border-bottom: ${(props) =>
+    props.activeProfile ? "none" : "1px solid gray"};
   width: 100%;
   box-sizing: border-box;
+  gap: 2rem;
 `;
 
 const Logo = styled.h1`
@@ -22,11 +26,37 @@ const SignInLink = styled(Link)`
   color: black;
 `;
 
-function Header() {
+const NavigationMenu = styled.nav`
+  display: flex;
+  gap: 2rem;
+  flex-grow: 1;
+`;
+
+const NavItem = styled(Link)`
+  color: white;
+  text-decoration: none;
+`;
+
+function Header(props) {
+  const { activeProfile } = props;
+
   return (
-    <HeaderSection>
+    <HeaderSection activeProfile={activeProfile}>
       <Logo>WiTFLiX</Logo>
-      <SignInLink to="/welcome">Sign in</SignInLink>
+      {activeProfile ? (
+        <>
+          <NavigationMenu>
+            <NavItem to="/welcome">item 1</NavItem>
+            <NavItem to="/welcome">item 1</NavItem>
+            <NavItem to="/welcome">item 1</NavItem>
+            <NavItem to="/welcome">item 1</NavItem>
+            <NavItem to="/welcome">item 1</NavItem>
+          </NavigationMenu>
+          <UserPanel activeProfile={activeProfile} />
+        </>
+      ) : (
+        <SignInLink to="/welcome">Sign in</SignInLink>
+      )}
     </HeaderSection>
   );
 }
